7ce2a0cb8ded0da714791229477f36ddd90f26f1
steve1994 <steve.harnadi@gmail.com>
2019-10-01 22:18:09 +0700

    Finish Bread Feature Users

--- pms/routes/users.js
+++ pms/routes/users.js
@@ -88,5 +88,85 @@ module.exports = (pool) => {
         })
     });
 
+    router.get('/add', helper.isLoggedIn, function (req,res) {
+        res.render('users/add');
+    })
+
+    router.post('/add', helper.isLoggedIn, function (req,res) {
+        let firstName = req.body.first_name;
+        let lastName = req.body.last_name;
+        let fullName = (firstName + ' ' + lastName).trim();
+        let email = req.body.email;
+        let password = req.body.password;
+        let role = req.body.position;
+        let isfulltime = (req.body.status == 'fulltime') ? true : false;
+        let isadmin = (req.body.isadmin == 'admin') ? true : false;
+
+        bcrypt.hash(password, saltRounds, function (err,hash) {
+            if (err) throw err;
+            let sql = `INSERT INTO users(firstname,lastname,fullname,email,password,role,isfulltime,isadmin) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`;
+            pool.query(sql,[firstName,lastName,fullName,email,hash,role,isfulltime,isadmin], function (err,response) {
+                if (err) throw err;
+                res.redirect('/users');
+            })
+        })
+    })
+
+    router.get('/edit/:iduser',helper.isLoggedIn,function (req,res) {
+        let idUser = req.params.iduser;
+        let sql = `SELECT * FROM users WHERE userid = ${idUser}`;
+        pool.query(sql, function (err,response) {
+            if (err) throw err;
+            let userData = response.rows[0];
+            if (req.session.user.userid == idUser) {
+                res.redirect('/users');
+            } else {
+                res.render('users/edit',{idUser,userData});
+            }
+        })
+    })
+
+    router.post('/edit/:iduser',helper.isLoggedIn,function (req,res) {
+        let idUser = req.params.iduser;
+        let firstName = req.body.first_name;
+        let lastName = req.body.last_name;
+        let fullName = (firstName + ' ' + lastName).trim();
+        let email = req.body.email;
+        let password = req.body.password;
+        let role = req.body.position;
+        let isfulltime = (req.body.status == 'fulltime') ? true : false;
+        let isadmin = (req.body.isadmin == 'admin') ? true : false;
+
+        if (password == '') {
+            let sql = `UPDATE users SET firstname=$1,lastname=$2,fullname=$3,email=$4,role=$5,isfulltime=$6,isadmin=$7 WHERE userid=${idUser}`;
+            pool.query(sql,[firstName,lastName,fullName,email,role,isfulltime,isadmin],function (err,response) {
+                if (err) throw err;
+                res.redirect('/users');
+            })
+        } else {
+            let sql = `UPDATE users SET firstname=$1,lastname=$2,fullname=$3,email=$4,password=$5,role=$6,isfulltime=$7,isadmin=$8 WHERE userid=${idUser}`;
+            bcrypt.hash(password, saltRounds, function (err,hash) {
+                if (err) throw err;
+                pool.query(sql,[firstName,lastName,fullName,email,hash,role,isfulltime,isadmin],function (err,response) {
+                    if (err) throw err;
+                    res.redirect('/users');
+                })
+            })
+        }
+    })
+
+    router.get('/delete/:iduser',helper.isLoggedIn, function (req,res) {
+        let idUser = req.params.iduser;
+        if (req.session.user.userid == idUser) {
+            res.redirect('/users');
+        } else {
+            let sql = `DELETE FROM users WHERE userid=${idUser}`;
+            pool.query(sql, function (err,response) {
+                if (err) throw err;
+                res.redirect('/users');
+            })
+        }
+    })
+
     return router;
 }
